7859df4a2e4f3c28b253e62199d41eefc97b35c6
Eugene Kalinin <kalinin.eugene@gmail.com>
2014-03-12 10:53:35 +0400

    Removed src directory

---
+++ uploader-java-validator/src/test/java/ru/ttk/crm/uploader/validator/TestXmlValidator.java
@@ -0,0 +1,11 @@
+package ru.ttk.crm.uploader.validator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: root
+ * Date: 3/12/14
+ * Time: 9:01 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestXmlValidator {
+}
